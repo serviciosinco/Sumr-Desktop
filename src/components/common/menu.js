a1d7b35569555534045eabb3683370af713b5731
camilo.garzon <camilo.garzon@servicios.in>
2021-01-22 21:35:10 -0500

    Update menu.js

--- src/components/common/menu.js
+++ src/components/common/menu.js
@@ -18,159 +18,107 @@ let i_dvlp = nativeImage.createFromPath( GetGlobal('main_icon_dvlp_refresh') )
 
 const setBar = (p)=>{
 	
-	
-	var m = [];
-	
-	m[0] = { 
-		label: 'Menu',
-        submenu: [
-            {
-                label: i18n.__('about_app'),
-                click:()=>{ open('http://sumr.in/'); }
-            }
-		]};
-		
-		/*
-		m[0].submenu[3] = { type: 'separator'};
-		m[0].submenu[4] = { label: i18n.__('new_window'),
-		   					accelerator:process.platform == 'darwin' ? 'Command+Shift+N' : 'Ctrl+Shift+N',
-			  				click:()=>{ createWindow({ main:'ok' }); }
-	    				  };
-	    */				  
-	    				  
-    if(config.get('menu_main_clients')=='ok'){
-	   	
-	    m[0].submenu[5] = { type: 'separator'};
-	    m[0].submenu[6] = { label: i18n.__('client_list'),
-		   					accelerator:process.platform == 'darwin' ? 'Command+Shift+A' : 'Ctrl+Shift+A',
-			  				click:()=>{ LoadContent({ u:GoToAccounts() }) }
-	    				  };
-	
-	}
-	    
-	    m[0].submenu[7] = { type: 'separator'};
-	    m[0].submenu[8] = { label: i18n.__('close_app'),
-			  				click:()=>{ app.quit(); }
-	    				  };				  
-	    
-	    
-    
-    
-     m[1] = {
-	    label: i18n.__('label_edit'),
+	const isMac = process.platform === 'darwin'
+
+	const template = [
+	// { role: 'appMenu' }
+	...(isMac ? [{
+		label: app.name,
 		submenu: [
-	        { label: i18n.__('edit_undo'), accelerator: "CmdOrCtrl+Z", selector: "undo:" },
-	        { label: i18n.__('edit_redo'), accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
-	        { type: "separator" },
-	        { label: i18n.__('edit_cut'), accelerator: "CmdOrCtrl+X", selector: "cut:" },
-	        { label: i18n.__('edit_copy'), accelerator: "CmdOrCtrl+C", selector: "copy:" },
-	        { label: i18n.__('edit_paste'), accelerator: "CmdOrCtrl+V", selector: "paste:" },
-	        { label: i18n.__('edit_slall'), accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
-		]};
-    
-     
-    m[2] = {
-        label: i18n.__('label_language'),
-        submenu: [
-            {
-                label: 'Español',
-                click:()=>{ config.set('lng', 'es'); }
-            }, 
-            { type: 'separator' }, 
-            {
-                label: 'English',
-                click:()=>{ config.set('lng', 'en'); }
-            }, 
-            { type: 'separator' }, 
-            {
-                label: 'Français',
-                click:()=>{ config.set('lng', 'fr'); }
-            }
-        ]};   
-	
-	if(config.get('menu_dvlp')=='ok'){
-		
-		m[3] = {
-	        label: i18n.__('label_developer'),
-	        id:'mn-developer',
-	        enabled:true,
-	        submenu: [
-	            {
-	                label:i18n.__('developer_refresh'),
-	                icon:i_dvlp,
-	                click:()=>{ Refresh(); }
-	            },
-	            { type: 'separator' }, 
-	            {
-	                label:i18n.__('delete_cache'),
-	                click:()=>{ ClearCache(); }
-	            },
-	            { type: 'separator' }, 
-	            {
-		            type:'checkbox',
-	                label:i18n.__('developer_showsv'),
-	                checked:(config.get('menu_dvlp_sv')=='ok'?true:false),
-	                click:()=>{ 
-		                
-		                if(config.get('menu_dvlp_sv') == 'ok'){
-			                config.set('menu_dvlp_sv', null); 
-			            }else{
-		                	config.set('menu_dvlp_sv', 'ok'); 
-		                }
-		                
-		                Refresh({ dvlp:'ok' });
-		                setBar();
-		                
-		            }
-	            }, 
-	            
-	            
-	            { type: 'separator' }, 
-	            {
-		            type:'checkbox',
-	                label:i18n.__('developer_test'),
-	                checked:(config.get('menu_dvlp_test')=='ok'?true:false),
-	                click:()=>{ 
-		                
-		                if(config.get('menu_dvlp_test') == 'ok'){
-			                config.set('menu_dvlp_test', null); 
-			            }else{
-		                	config.set('menu_dvlp_test', 'ok'); 
-		                }
-		                
-		                Refresh({ dvlp:'ok' });
-		                setBar();
-		                
-		            }
-	            }, 
-	            
-	            
-	            { type: 'separator' }, 
-	            {
-	                label:i18n.__('developer_tools'),
-	                submenu:[
-	                {
-		            	label:i18n.__('developer_tools_free'),
-						click:()=>{ mWin.webContents.openDevTools({ mode:'detach' }) }    
-	                },
-	                {
-		            	label:i18n.__('developer_tools_right'),
-						click:()=>{ mWin.webContents.openDevTools({ mode:'right' }) }    
-	                },
-	                {
-		            	label:i18n.__('developer_tools_left'),
-						click:()=>{ mWin.webContents.openDevTools({ mode:'left' }) }    
-	                }]
-	            }
-	        ]};
+		{ role: 'about' },
+		{ type: 'separator' },
+		{ role: 'services' },
+		{ type: 'separator' },
+		{ role: 'hide' },
+		{ role: 'hideothers' },
+		{ role: 'unhide' },
+		{ type: 'separator' },
+		{ role: 'quit' }
+		]
+	}] : []),
+	// { role: 'fileMenu' }
+	{
+		label: 'File',
+		submenu: [
+		isMac ? { role: 'close' } : { role: 'quit' }
+		]
+	},
+	// { role: 'editMenu' }
+	{
+		label: 'Edit',
+		submenu: [
+		{ role: 'undo' },
+		{ role: 'redo' },
+		{ type: 'separator' },
+		{ role: 'cut' },
+		{ role: 'copy' },
+		{ role: 'paste' },
+		...(isMac ? [
+			{ role: 'pasteAndMatchStyle' },
+			{ role: 'delete' },
+			{ role: 'selectAll' },
+			{ type: 'separator' },
+			{
+			label: 'Speech',
+			submenu: [
+				{ role: 'startSpeaking' },
+				{ role: 'stopSpeaking' }
+			]
+			}
+		] : [
+			{ role: 'delete' },
+			{ type: 'separator' },
+			{ role: 'selectAll' }
+		])
+		]
+	},
+	// { role: 'viewMenu' }
+	{
+		label: 'View',
+		submenu: [
+		{ role: 'reload' },
+		{ role: 'forceReload' },
+		{ role: 'toggleDevTools' },
+		{ type: 'separator' },
+		{ role: 'resetZoom' },
+		{ role: 'zoomIn' },
+		{ role: 'zoomOut' },
+		{ type: 'separator' },
+		{ role: 'togglefullscreen' }
+		]
+	},
+	// { role: 'windowMenu' }
+	{
+		label: 'Window',
+		submenu: [
+		{ role: 'minimize' },
+		{ role: 'zoom' },
+		...(isMac ? [
+			{ type: 'separator' },
+			{ role: 'front' },
+			{ type: 'separator' },
+			{ role: 'window' }
+		] : [
+			{ role: 'close' }
+		])
+		]
+	},
+	{
+		role: 'help',
+		submenu: [
+		{
+			label: 'Learn More',
+			click: async () => {
+			const { shell } = require('electron')
+			await shell.openExternal('https://electronjs.org')
+			}
+		}
+		]
 	}
-		
-	const menuTemplate = m;
-	
-	var menu = Menu.buildFromTemplate(menuTemplate)
+	]
+
+	const menu = Menu.buildFromTemplate(template)
 	Menu.setApplicationMenu(menu)
-	
-	return true;
 
 }
 
